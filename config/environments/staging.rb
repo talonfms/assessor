3d41a1f28d0f77532eea636c0d12dbe6920a412d
Harriet Oughton <harrietoughton@gmail.com>
2025-04-08 12:25:15 +0100

    HC-13 Ensure that email links are pointing to the correct URL

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = {host: Jumpstart.config.domain}
+  config.action_mailer.default_url_options = {host: "https://zivio-assessor-staging-43af1f348725.herokuapp.com/"}
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   config.action_mailer.smtp_settings = {
