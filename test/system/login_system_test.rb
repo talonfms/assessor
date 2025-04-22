b8fa48d020d5f3614c5b84aa7dc5a4adcdf4b2c9
Harriet Oughton <harrietoughton@gmail.com>
2025-04-22 18:47:04 +0200

    HC-38 Fix flaky by updating password string

--- test/system/login_system_test.rb
+++ test/system/login_system_test.rb
@@ -7,50 +7,50 @@ class LoginSystemTest < ApplicationSystemTestCase
   end
 
   test "can login" do
-    login_with_email_and_password users(:one).email, "password"
+    login_with_email_and_password users(:one).email, "P@ssw0rd64646464"
     assert_selector "p", text: I18n.t("devise.sessions.signed_in")
   end
 
   test "handles invalid email" do
-    login_with_email_and_password "missing@example.org", "password"
+    login_with_email_and_password "missing@example.org", "P@ssw0rd64646464"
     assert_selector "p", text: I18n.t("devise.failure.invalid", authentication_keys: "Email")
   end
 
-  # test "two factor required" do
-  #   login_with_email_and_password users(:twofactor).email, "password"
-  #   assert_selector "h1", text: I18n.t("users.two_factor.header")
-  # end
+  test "two factor required" do
+    login_with_email_and_password users(:twofactor).email, "P@ssw0rd64646464"
+    assert_selector "h1", text: I18n.t("users.two_factor.header")
+  end
 
-  # test "two factor success with otp password" do
-  #   login_with_email_and_password users(:twofactor).email, "password"
-  #   submit_otp users(:twofactor).current_otp
-  #   assert_selector "p", text: I18n.t("devise.sessions.signed_in")
-  # end
+  test "two factor success with otp password" do
+    login_with_email_and_password users(:twofactor).email, "P@ssw0rd64646464"
+    submit_otp users(:twofactor).current_otp
+    assert_selector "p", text: I18n.t("devise.sessions.signed_in")
+  end
 
-  # test "two factor success with otp backup code" do
-  #   user = users(:twofactor)
-  #   login_with_email_and_password user.email, "password"
-  #   submit_otp user.otp_backup_codes[0]
-  #   assert_selector "p", text: I18n.t("devise.sessions.signed_in")
-  # end
+  test "two factor success with otp backup code" do
+    user = users(:twofactor)
+    login_with_email_and_password user.email, "P@ssw0rd64646464"
+    submit_otp user.otp_backup_codes[0]
+    assert_selector "p", text: I18n.t("devise.sessions.signed_in")
+  end
 
-  # test "two factor fails with bad input" do
-  #   login_with_email_and_password users(:twofactor).email, "password"
-  #   submit_otp "invalid"
-  #   assert_selector "p", text: I18n.t("users.sessions.create.incorrect_verification_code")
-  # end
+  test "two factor fails with bad input" do
+    login_with_email_and_password users(:twofactor).email, "P@ssw0rd64646464"
+    submit_otp "invalid"
+    assert_selector "p", text: I18n.t("users.sessions.create.incorrect_verification_code")
+  end
 
-  # test "two factor always enforced for separate user logins" do
-  #   login_with_email_and_password users(:twofactor).email, "password"
-  #   submit_otp "invalid"
-  #   assert_selector "p", text: I18n.t("users.sessions.create.incorrect_verification_code")
+  test "two factor always enforced for separate user logins" do
+    login_with_email_and_password users(:twofactor).email, "P@ssw0rd64646464"
+    submit_otp "invalid"
+    assert_selector "p", text: I18n.t("users.sessions.create.incorrect_verification_code")
 
-  #   second_user = users(:twofactor).dup
-  #   second_user.update!(email: "twofactor2@example.org", password: "abcd1234", password_confirmation: "abcd1234", terms_of_service: true)
-  #   login_with_email_and_password second_user.email, "abcd1234"
-  #   submit_otp "invalid"
-  #   assert_selector "p", text: I18n.t("users.sessions.create.incorrect_verification_code")
-  # end
+    second_user = users(:twofactor).dup
+    second_user.update!(email: "twofactor2@example.org", password: "abcd1234", password_confirmation: "abcd1234", terms_of_service: true)
+    login_with_email_and_password second_user.email, "abcd1234"
+    submit_otp "invalid"
+    assert_selector "p", text: I18n.t("users.sessions.create.incorrect_verification_code")
+  end
 
   private
 
